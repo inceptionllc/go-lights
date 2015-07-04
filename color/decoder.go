4d94bd2b1fc1b2221e7cf74946ce02688c0151b7
Iain Shigeoka <iain@shigeoka.com>
2015-07-04 16:30:19 -0700

    Move schedule parser to schedule package

--- color/decoder.go
+++ color/decoder.go
@@ -3,7 +3,6 @@ package color
 import (
 	"fmt"
 	"image/color"
-	"io"
 	"strconv"
 )
 
@@ -45,55 +44,4 @@ func ParseColorCode(colorCode string) (color.Color, error) {
 	default:
 		return nil, fmt.Errorf("Color code must be 4 or 6 characters - found %d", len(colorCode))
 	}
-}
-
-// ParseScheduleFile reads in a json encoded schedule and produces a Config.
-func ParseScheduleFile(r io.ReadCloser) ([]*Schedule, error) {
-	return nil, nil
-	/*
-		schedules := make([]*schedule.Schedule, 0, 1)
-		patterns := make([]*schedule.Pattern, 0, 1)
-
-		var jsonObj SObj
-
-		decoder := json.NewDecoder(r)
-		err := decoder.Decode(&jsonObj)
-		if err != nil {
-			return schedules, fmt.Errorf("Error parsing schedule obj JSON: %s", err)
-		}
-
-		for _, pObj := range jsonObj.Patterns {
-			p := schedule.NewPattern()
-			p.name = pObj.Name
-			for _, pSlotObj := range pObj.Slots {
-				col, err := ParseColorCode(pSlotObj.Color)
-				if err != nil {
-					return schedules, fmt.Errorf("Error parsing color code: %s", pSlotObj.Color)
-				}
-				p.AddPatternSlot(schedule.NewPatternSlot(col, pSlotObj.Hold, pSlotObj.Transition))
-			}
-			patterns = append(patterns, p)
-		}
-
-		for _, sObj := range jsonObj.Schedules {
-			s := schedule.NewSchedule()
-			p, err := schedule.findPattern(patterns, sObj.Pattern)
-			if err != nil {
-				return schedules, err
-			}
-
-			s.pattern = p
-			s.mon = sObj.Mon
-			s.tue = sObj.Tue
-			s.wed = sObj.Wed
-			s.thu = sObj.Thu
-			s.fri = sObj.Fri
-			s.sat = sObj.Sat
-			s.sun = sObj.Sun
-
-			schedules = append(schedules, s)
-		}
-
-		return schedules, nil
-	*/
 }
